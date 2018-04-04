3fe472ded699ccb43a74c7e19bd742e97c161e0e
Usuario <Usuario@DESKTOP-F2Q933M>
2018-04-04 16:07:04 -0300

    Criação de uma interface para uso do métodos na classe Data

--- AlterarData/Data.cs
+++ AlterarData/Data.cs
@@ -3,7 +3,7 @@ using System.Data;
 
 namespace AlterarData
 {
-    class Data
+    class Data : InterfaceData
     {
         #region Propriedades
 
---
+++ AlterarData/InterfaceData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlterarData
+{
+    interface InterfaceData
+    {
+        string ChangeDate(string date, char op, long value);
+        bool ValidarDiaTextBox(int diaParaChecar);
+        bool ValidarMesTextBox(int mesParaChecar);
+        bool ValidarDiaMes(int dia, int mes);
+        bool ValidarHoraTextBox(int horaParaChecar);
+        bool ValidarMinutosTextBox(int minutosParaChecar);
+     }
+}
